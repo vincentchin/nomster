4e1daf7b150a70e6ffd545dd683970c3a80965ef
Vincent Chin <vincent.ch91@gmail.com>
2015-07-12 20:37:28 +0000

    Added validation for editing and update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :des
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, :status => :unprocessable_entity
+		end
 	end
 
 	def show
@@ -35,7 +39,12 @@ before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :des
 		end	
 
 		@place.update_attributes(place_params)
-		redirect_to root_path
+
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :edit, :status => :unprocessable_entity
+		end
 	end
 
 	def destroy
