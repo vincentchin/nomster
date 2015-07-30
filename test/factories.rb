3d5e1ce5f641f9af2a9a2f5f947e867b3ae778bd
Federico Ramallo <framallo@gmail.com>
2015-07-30 13:53:42 -0500

    fixed comments test

--- test/factories.rb
+++ test/factories.rb
@@ -2,6 +2,17 @@ FactoryGirl.define do
   factory :user do
    email "vincent.ch91@gmail.com"
    password "password"
+
+   factory :owner do
+     email "owner@place.com"
+   end
+  end
+
+  factory :place do
+   name      'Home'
+   description 'home sweet home'
+   address   '6903 3rd Avenue, Brooklyn, NY 11209'
+   association :user, factory: :owner
   end
 
 end
